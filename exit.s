1fcf73eb392d7bc5aab1782ffb85f7bf0493a010
Parker Smith <parker@parkersmith.us>
2018-03-27 17:10:00 -0600

    Write exit.s

---
+++ exit.s
@@ -0,0 +1,22 @@
+#PURPOSE:  Simple program that exits and returns 
+#	   a status code back to the Linux kernel
+
+#INPUT:    none
+#
+
+#OUTPUT:   returns a status code.
+
+#VARIABLES:
+#	   %eax holds the system call number
+#	   %ebx holds the return status
+
+.section .data
+
+.section .text
+.globl _start
+_start:
+movl $1, %eax
+
+movl $0, %ebx
+
+int $0x80
